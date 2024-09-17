0a43a24ff8283af3c66f4eeddc7990867ffae333
Danilo Costa <danilodalessandro08@gmail.com>
2024-09-17 11:19:52 -0300

    ajuste no app orçamentos

--- aid/forms.py
+++ aid/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import AuxilioColaborador
+from .models import AuxilioColaborador, LinhaOrcamentaria
 
 class AuxilioColaboradorForm(forms.ModelForm):
     class Meta:
@@ -9,13 +9,19 @@ class AuxilioColaboradorForm(forms.ModelForm):
             'valor_parcela', 'qtd_parcelas', 'obs', 'mes_inicio', 'previsao_inicio'
         ]
         widgets = {
-            'beneficiado': forms.Select(attrs={'class': 'form-control'}),  # Campo de seleção para o colaborador
-            'linha': forms.Select(attrs={'class': 'form-control'}),  # Campo de seleção para a linha orçamentária
-            'tipo': forms.Select(attrs={'class': 'form-control'}),  # Campo de seleção para o tipo de auxílio
-            'beneficio': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Informe o benefício'}),  # Campo de texto para o benefício
-            'valor_parcela': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Valor da parcela'}),  # Campo numérico para valor por parcela
-            'qtd_parcelas': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Quantidade de parcelas'}),  # Campo numérico para quantidade de parcelas
-            'obs': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Observações (opcional)'}),  # Campo de texto para observações
-            'mes_inicio': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),  # Campo de data com seletor de calendário
-            'previsao_inicio': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),  # Campo de data com seletor de calendário
+            'beneficiado': forms.Select(attrs={'class': 'form-control'}),
+            'linha': forms.Select(attrs={'class': 'form-control'}),
+            'tipo': forms.Select(attrs={'class': 'form-control'}),
+            'beneficio': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Informe o benefício'}),
+            'valor_parcela': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Valor da parcela'}),
+            'qtd_parcelas': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Quantidade de parcelas'}),
+            'obs': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Observações (opcional)'}),
+            'mes_inicio': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'previsao_inicio': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Filtrar o campo linha para mostrar apenas linhas com classe CAPEX
+        if 'linha' in self.fields:
+            self.fields['linha'].queryset = LinhaOrcamentaria.objects.filter(classe='CAPEX')
